e354c0acd032aa7d1b23b1b9b0b5a5a2c7bc021f
Muzammel Osmani Azizi <ms7.swaggy@gmail.com>
2024-04-01 11:00:48 -0700

    completed

---
+++ ASP.NETMVC/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+using ASP.NETMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NETMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var names = new List<string>();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ASP.NETMVC/Data/IProductRepository.cs
@@ -0,0 +1,23 @@
+﻿using ASP.NETMVC.Models;
+
+namespace ASP.NETMVC.Data
+{
+    public interface IProductRepository
+    {
+       
+        public IEnumerable<Product> GetAllProducts();
+
+        public Product GetProduct(int id);
+
+        public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+
+        public IEnumerable<Category> GetCategories();
+
+        public Product AssignCategory();
+
+        public void DeleteProduct(Product product);
+    }
+
+}
